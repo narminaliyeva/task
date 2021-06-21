719fd4de761a67c9bf22c71b3a04bf81023b8083
narminaliyeva <narminaliyeva.00@mail.ru>
2021-06-21 16:39:55 +0400

    First commit

---
+++ Cyclic number/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Cyclic_number
+{
+    class Program
+    {
+        public static string SortString(string inputString)
+        {
+            char[] tempArray = inputString.ToCharArray();
+            Array.Sort(tempArray);
+            return new string(tempArray);
+        }
+
+        public static bool SameDigit(int num1, int num2)
+        {
+            string str1 = num1.ToString();
+            string str2 = num2.ToString();
+            if (SortString(str1) == SortString(str2))
+                return true;
+
+            return false;
+        }
+
+        public static bool SameDigitArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!SameDigit(arr[0], arr[i]))
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            for (int num = 100000; num < (int)Math.Pow(10, 6) / 6; num++)
+            {
+                int[] nums = { num, num * 2, num * 3, num * 4, num * 5, num * 6 };
+
+                if (SameDigitArr(nums))
+                    Console.WriteLine("This number is :" + num);
+            }
+
+            watch.Stop();
+            Console.WriteLine($"Execution time {watch.ElapsedMilliseconds} ms");
+
+        }
+    }
+}
